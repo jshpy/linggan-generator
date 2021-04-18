4a69e4362537bb0d138165240e33b9ef11778a82
张旭 <jshpy@163.com>
2021-04-18 19:13:19 +0800

    done ui

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import "./style.scss"
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack', 'aaaa'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
