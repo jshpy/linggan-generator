efda980c6e4ccbb69b635d65f84af74fe0c18016
张旭 <jshpy@163.com>
2021-04-18 21:01:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./style.scss"
+import QrImg from "../assets/img/QR.jpg"
 
+// 添加二维码
+(()=>{
+  const imgWarp = document.getElementById("wchat-qr")
+  const img = document.createElement("img")
+  img.src=QrImg
+  imgWarp.style.visibility='hidden';
+  imgWarp.appendChild(img)
+  const wchatIcon = document.getElementById("wchat")
+  wchatIcon.onmouseover = function(e) {
+    imgWarp.style.visibility='visible';
+  }
+  wchatIcon.onmouseleave = function(e) {
+    imgWarp.style.visibility='hidden';
+  }
+})()
